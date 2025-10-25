d08372073346c5ee17f8b0979cdb04cd45d67321
harsha-050 <harshamajji157@gmail.com>
2025-10-26 03:18:35 +0530

    Fix CORS for Node.js API

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: [
+    'https://harsha-050.github.io',
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
   credentials: true
 }));
 app.use(express.json());
