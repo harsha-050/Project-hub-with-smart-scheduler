8831bcef2a388a37ed473ce918fd8f739bd3192e
harsha-050 <harshamajji157@gmail.com>
2025-10-26 02:04:26 +0530

    Setup GitHub Actions deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Project-2/', // Replace 'Project-2' with your GitHub repository name
+  base: '/',
   server: {
     port: 5173,
     proxy: {
