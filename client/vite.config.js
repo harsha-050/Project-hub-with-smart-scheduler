71979afde852617fdaf5af17797003b3ef8308f7
harsha-050 <harshamajji157@gmail.com>
2025-10-26 02:09:56 +0530

    Update base path for GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Project-hub-with-smart-scheduler/',
   server: {
     port: 5173,
     proxy: {
